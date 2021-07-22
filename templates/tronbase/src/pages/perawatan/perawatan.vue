86b7046a5fe93d0e5352e86f43c03807dc045424
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-07-22 07:45:35 +0700

    Penyesuaian tata letak fasilitas kedalam grid

--- templates/tronbase/src/pages/perawatan/perawatan.vue
+++ templates/tronbase/src/pages/perawatan/perawatan.vue
@@ -1,6 +1,27 @@
 <template>
-  <q-page class="flex flex-center">
-    <h3>Halaman Perawatan</h3>
+  <q-page padding>
+    <h4 class="text-center text-uppercase">Fasilitas BBTA3</h4>
+
+    <div class="row q-gutter-md justify-around">
+      <div class="col-12 col-md-5 q-pa-md" v-for="f in fasilitas" :key="f.gambar">
+        <q-card flat bordered style="height: 100%;">
+          <q-img :src="f.gambar" />
+
+          <q-card-section>
+            <div class="text-overline text-orange-9">{{ f.singkatan }}</div>
+            <div class="text-h5 q-mt-sm q-mb-xs">{{ f.nama }}</div>
+            <div class="text-body1 text-grey">
+              {{ f.deskripsi }}
+            </div>
+          </q-card-section>
+
+          <q-card-actions>
+            <q-btn flat color="primary" label="Status Perawatan" @click="kunjungiStatus(f.status)" />
+            <q-btn flat color="dark" label="Kegiatan Perawatan" />
+          </q-card-actions>
+        </q-card>
+      </div>
+    </div>
   </q-page>
 </template>
 
