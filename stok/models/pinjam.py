f41374c200524de56c7f74b6a3a0131da8896928
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-05 17:57:35 +0700

    Membuat model database untuk transaksi pinjam

---
+++ stok/models/pinjam.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+from stok.models.barang import Barang
+
+
+class Pinjam(models.Model):
+    barang = models.ForeignKey(Barang, on_delete=models.CASCADE)
+    jumlah = models.IntegerField()
+    dipinjam = models.ForeignKey(User, on_delete=models.CASCADE)
+    kapan = models.DateTimeField(auto_now_add=True)
+    dikembalikan = models.DateTimeField(blank=True)
+
+    class Meta:
+        verbose_name_plural = "Pinjam"
+        indexes = [
+            models.Index(fields=["barang", "dipinjam"])
+        ]
+
+    def __str__(self):
+        return f"{self.barang.nama} dipinjam oleh {self.dipinjam.username}"
