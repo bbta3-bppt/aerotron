2ff3cc76d59c0c3eb311270e05eaf3c6f9309786
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-06 22:01:04 +0700

    Periksa cookie otentikasi setiap perpindahan router

--- templates/tronbase/src/boot/oten.js
+++ templates/tronbase/src/boot/oten.js
@@ -1,13 +1,19 @@
-import { boot } from 'quasar/wrappers'
+import {boot} from "quasar/wrappers"
+import {Cookies} from "quasar"
 
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
-export default boot(async ({ store, router}) => {
+export default boot(async ({store, router}) => {
   router.beforeEach(async (to, from, next) => {
     const kunci = to.matched.some(value => value.meta["kunci"])
 
     if (kunci) {
-      next({name: 'masuk'})
+      if (Cookies.has("_msk")) {
+        next()
+      } else {
+        next({name: "masuk"})
+      }
+
     } else {
       next()
     }
