e3d84b7ba04c1a20b5c03b931884ea07b49b48bc
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-07 10:31:20 +0700

    Penerapan hook kedalam vue composable api

--- templates/tronbase/src/pages/oten/otentikasi.js
+++ templates/tronbase/src/pages/oten/otentikasi.js
@@ -1,5 +1,7 @@
-import {defineComponent, ref} from "vue"
+import {useStore} from "vuex"
+import {useRouter} from "vue-router"
 import {Cookies, Notify} from "quasar"
+import {defineComponent, ref} from "vue"
 
 
 export default defineComponent({
@@ -10,71 +12,81 @@ export default defineComponent({
     }
   },
   setup() {
+    const router = useRouter()
+    const store = useStore()
     const username = ref(null)
     const usernameRef = ref(null)
+    const usernameRules = [
+      val => (val && val.length > 0) || 'Username harus diisi'
+    ]
+
     const password = ref(null)
     const passwordRef = ref(null)
+    const passwordRules = [
+      val => (val !== null && val !== '') || 'Password harus diisi'
+    ]
+
+    const onSubmit = async () => {
+      usernameRef.value.validate()
+      passwordRef.value.validate()
+
+      if (!usernameRef.value.hasError && !passwordRef.value.hasError) {
+        try {
+          const res = await store.dispatch("otentikasi/getTokenAction", {
+            username: username.value,
+            password: password.value
+          })
+          const token = res.data
+
+          Cookies.set("_msk", token.access, {
+            sameSite: 'Lax',
+            expires: '1d'
+          })
+
+          Cookies.set("_mskr", token.refresh, {
+            sameSite: 'Lax',
+            expires: '1d'
+          })
+
+          router.push({path: "/"}).then(() => {})
+        } catch (err) {
+          let message = null
+
+          if (err.response) {
+            message = err.response.data.detail
+          } else if (err.request) {
+            message = err.request
+          } else {
+            message = err.message
+          }
+
+          Notify.create({
+            type: "negative",
+            message: message
+          })
+        }
+      }
+    }
+
+    const onReset = () => {
+      username.value = null
+      password.value = null
+
+      usernameRef.value.resetValidation()
+      passwordRef.value.resetValidation()
+    }
 
     return {
       username,
       usernameRef,
-      usernameRules: [
-        val => (val && val.length > 0) || 'Username harus diisi'
-      ],
+      usernameRules,
 
       password,
       passwordRef,
-      passwordRules: [
-        val => (val !== null && val !== '') || 'Password harus diisi'
-      ],
+      passwordRules,
 
-      async onSubmit() {
-        usernameRef.value.validate()
-        passwordRef.value.validate()
-
-        if (!usernameRef.value.hasError || !passwordRef.value.hasError) {
-          try {
-            const res = await this.$store.dispatch("otentikasi/getTokenAction", {
-              username: username.value,
-              password: password.value
-            })
-            const token = res.data
-
-            Cookies.set("_msk", token.access, {
-              sameSite: 'Lax'
-            })
-
-            Cookies.set("_mskr", token.refresh, {
-              sameSite: 'Lax',
-              expires: '1d'
-            })
-
-            this.$router.push({path: "/"}).then(() => {})
-          } catch (err) {
-            let message = null
-
-            if (err.response) {
-              message = err.response.data.detail
-            } else if (err.request) {
-              message = err.request
-            } else {
-              message = err.message
-            }
-
-            Notify.create({
-              type: "negative",
-              message: message
-            })
-          }
-        }
-      },
-      onReset() {
-        username.value = null
-        password.value = null
-
-        usernameRef.value.resetValidation()
-        passwordRef.value.resetValidation()
-      }
+      onSubmit,
+      onReset
     }
   }
 })
