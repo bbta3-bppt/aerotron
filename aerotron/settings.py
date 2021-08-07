cfdb24cf1dc5feabf3532977b6149830327866ad
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-07 10:31:37 +0700

    Modifikasi konfigurasi backend

--- aerotron/settings.py
+++ aerotron/settings.py
@@ -157,7 +157,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': False,
+    'ROTATE_REFRESH_TOKENS': True,
 }
 
 CORS_ORIGIN_ALLOW_ALL = False
