32b1110cd3ac2208cc0e9451eb6dbe292b174828
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-09 18:30:26 +0700

    Membuat class view dengan fungsi ambil data user

--- oten/views.py
+++ oten/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
 
-# Create your views here.
+from oten.serializers import UserSerializer
+
+
+class SelectedUser(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        selected_user = User.objects.filter(pk=self.request.user.pk)
+
+        return selected_user
