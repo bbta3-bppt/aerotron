172514ad3f11e19d683d21db69c1416cc475c09b
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-09 18:26:14 +0700

    Membuat fungsi action untuk ambil barang

--- templates/tronbase/src/store/stok/actions.js
+++ templates/tronbase/src/store/stok/actions.js
@@ -1,14 +1,14 @@
 import axios from "axios"
 import {Cookies} from "quasar"
 
-export async function barangAction (ctx, {page}) {
+export async function getBarangAction (ctx, {page}) {
   const header = Cookies.get("_msk")
 
   return await axios.get(
     `${window.location.protocol}//${process.env.REST_HOST}/stok/api/barang/`,
     {
       headers: { authorization: `Bearer ${header}` },
-      params: { page: page != null ? page : null }
+      params: { pages: page != null ? page : null }
     }
   )
 }
