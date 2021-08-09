f76adc90c4a63b50c9e90d7cadbadfdfbedebd7a
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-09 18:29:21 +0700

    Ubah pengaturan baris data per halaman

--- aerotron/settings.py
+++ aerotron/settings.py
@@ -141,7 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # https://www.django-rest-framework.org/#example
 
 REST_FRAMEWORK = {
-    'PAGE_SIZE': 100,
+    'PAGE_SIZE': 10,
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
