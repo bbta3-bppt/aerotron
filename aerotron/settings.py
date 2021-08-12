0aae6a72c318bb8fc22bd4d157d6b3c8a3435afb
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-12 16:18:30 +0700

    Pengaturan pagination melalui env

--- aerotron/settings.py
+++ aerotron/settings.py
@@ -141,7 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # https://www.django-rest-framework.org/#example
 
 REST_FRAMEWORK = {
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': os.environ['PAGE_SIZE'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
