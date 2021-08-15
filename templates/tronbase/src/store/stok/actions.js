41acc8571f29267e704f2e3bc4a8acab3f678394
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-15 17:36:36 +0700

    Membuat fungsi lanjut ambil kategori untuk action state

--- templates/tronbase/src/store/stok/actions.js
+++ templates/tronbase/src/store/stok/actions.js
@@ -12,3 +12,15 @@ export async function getBarangAction (ctx, {page}) {
     }
   )
 }
+
+export async function getKategoriAction (ctx, {page}) {
+  const header = Cookies.get("_msk")
+
+  return await axios.get(
+    `${window.location.protocol}//${process.env.REST_HOST}/stok/api/kategori/`,
+    {
+      headers: { authorization: `Bearer ${header}` },
+      params: { page: page != null ? page : null }
+    }
+  )
+}
