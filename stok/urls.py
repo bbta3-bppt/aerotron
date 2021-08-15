511b52af16efa9688d9c4d47b127d1a1a57887cb
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-15 17:40:43 +0700

    Mendaftarkan view baru kedalam url bagian kategori

--- stok/urls.py
+++ stok/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from stok.views.barang_view import BarangView
+from stok.views.kategori_view import GetKategoriView
 
 urlpatterns = [
     path('api/barang/', BarangView.as_view(), name='daftar_barang'),
+    path('api/kategori/', GetKategoriView.as_view(), name='daftar_kategori'),
 ]
