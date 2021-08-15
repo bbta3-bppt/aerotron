46b0b94737295a41e8d409d2dc5bc78a05cc8fe6
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-15 15:24:52 +0700

    Membuat fungsi awal state mutation kategori

--- templates/tronbase/src/store/stok/mutations.js
+++ templates/tronbase/src/store/stok/mutations.js
@@ -1,7 +1,21 @@
-export function setBarangMutation(state, {payload}) {
-  state.barang = payload
+export function setBarangMutation(state, {count, results, next, previous}) {
+  state.barang.count = count
+  state.barang.results = results
+  state.barang.next = next
+  state.barang.previous = previous
+}
+
+export function setKategoriMutation(state, {count, results, next, previous}) {
+  state.kategori.count = count
+  state.kategori.results = results
+  state.kategori.next = next
+  state.kategori.previous = previous
 }
 
 export function resetBarangMutation(state) {
-  state.barang = []
+  state.barang.results = []
+}
+
+export function resetKategoriMutation(state) {
+  state.kategori.results = []
 }
