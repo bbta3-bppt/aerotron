e8a1baaf14296c32488ff4f263d5bfcfdb133429
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-18 23:19:07 +0700

    Membuat komponen pencarian barang

---
+++ templates/tronbase/src/components/Stok/SearchBox/SearchBarang.vue
@@ -0,0 +1,23 @@
+<template>
+  <form
+    @submit.prevent.stop="onSearch"
+  >
+    <q-input
+      dense outlined
+      lazy-rules
+      v-model="computeTemukan"
+      color="green" label="Cari..."
+      :model-value="computeTemukan"
+    >
+      <template v-slot:prepend>
+        <q-icon name="eva-search-outline"/>
+      </template>
+
+      <template v-slot:append v-if="computeTemukan">
+        <q-icon name="cancel" @click.stop="onReset" class="cursor-pointer" />
+      </template>
+    </q-input>
+  </form>
+</template>
+
+<script src="./search.js" />
