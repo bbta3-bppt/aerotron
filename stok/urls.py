7ece8eac6620773399adde74190b92e2f1c41c97
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-21 19:34:26 +0700

    Mendaftarkan url untuk melihat pinjaman barang

--- stok/urls.py
+++ stok/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from stok.views.barang_view import BarangView, SelectedBarangView
 from stok.views.kategori_view import GetKategoriView
+from stok.views.pinjam_view import AmbilPinjamView
 
 urlpatterns = [
     path('api/barang/', BarangView.as_view(), name='daftar_barang'),
     path('api/barang/<int:pk>', SelectedBarangView.as_view(), name='barang_tertentu'),
+    path('api/barang/<int:pk_barang>/pinjaman/', AmbilPinjamView.as_view(), name='pinjaman_barang_tertentu'),
     path('api/kategori/', GetKategoriView.as_view(), name='daftar_kategori'),
 ]
