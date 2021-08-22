1488183b0b98faca14c4cfba446736d2c68c87a3
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-22 12:52:00 +0700

    Penambahan interaksi pagination data pinjaman

--- templates/tronbase/src/pages/stok/RincianStok/rincian.js
+++ templates/tronbase/src/pages/stok/RincianStok/rincian.js
@@ -13,6 +13,8 @@ export default {
     const route = useRoute()
     const pinjaman = ref([])
     const barang = ref({})
+    const pg = ref(2)
+    const dialogPinjaman = ref(false)
     const barangTertentu = computed({
       get() {
         return barang.value
@@ -27,21 +29,35 @@ export default {
 
       const paramsId = route.params["id"]
       const res = await getBarangTertentu(store, router, paramsId)
-      const resPinjaman = await getAllPinjamanTertentu(store, router, 1, paramsId)
-
       barangTertentu.value = res.data
-      pinjaman.value = resPinjaman
+
+      await getAllPinjamanTertentu(store, router, 1, paramsId)
+      pinjaman.value = store.getters["pinjaman/getAllPinjamanGetter"]
     })
 
     const onKembali = () => {
       router.go(-1)
     }
 
+    const onPinjaman = () => {
+      dialogPinjaman.value = true
+    }
+
+    const onMemuatPinajaman = async () => {
+      const paramsId = route.params["id"]
+
+      await getAllPinjamanTertentu(store, router, pg.value, paramsId)
+      pg.value += 1
+    }
+
     return {
+      dialogPinjaman,
       barangTertentu,
       pinjaman,
       moment,
-      onKembali
+      onKembali,
+      onMemuatPinajaman,
+      onPinjaman
     }
   }
 }
