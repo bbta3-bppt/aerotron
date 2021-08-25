da1564698b9e2afd77ed26edc676dbcb125b1dd6
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-25 11:13:31 +0700

    Membuat fungsi untuk ambil data fasilitas

--- templates/tronbase/src/pages/pengujian/pengujian.js
+++ templates/tronbase/src/pages/pengujian/pengujian.js
@@ -1,6 +1,30 @@
-import {defineComponent} from "vue"
+import {defineComponent, onMounted, ref} from "vue"
+import {getAllFasilitas} from "src/services/fasilitas"
+import {useStore} from "vuex"
+import {useRouter} from "vue-router"
 
 
 export default defineComponent({
   name: 'HalamanPengujian',
+  setup() {
+    const store = useStore()
+    const router = useRouter()
+    const fasilitas = ref({})
+    const pg = ref(2)
+
+    onMounted(async () => {
+      await getAllFasilitas(store, router, 1)
+      fasilitas.value = store.getters["fasilitas/getFasilitasGetter"]
+    })
+
+    const onMuatLagi = async () => {
+      await getAllFasilitas(store, router, pg.value)
+      pg.value += 1
+    }
+
+    return {
+      fasilitas,
+      onMuatLagi
+    }
+  }
 })
