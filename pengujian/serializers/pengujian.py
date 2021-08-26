ba759336bc84b2483fca62b7e12186064a4856ec
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-26 20:15:46 +0700

    Menyesuaikan formasi serializers pengujian

--- pengujian/serializers/pengujian.py
+++ pengujian/serializers/pengujian.py
@@ -5,6 +5,7 @@ from pengujian.models.pengujian import Pengujian
 
 class PengujianSerializers(serializers.ModelSerializer):
     nama_fasilitas = serializers.SerializerMethodField()
+    nama_pemilik = serializers.SerializerMethodField()
 
     class Meta:
         model = Pengujian
@@ -13,3 +14,10 @@ class PengujianSerializers(serializers.ModelSerializer):
     @staticmethod
     def get_nama_fasilitas(obj):
         return obj.fasilitas.nama
+
+    @staticmethod
+    def get_nama_pemilik(obj):
+        if obj.pemilik.first_name or obj.pemilik.last_name:
+            return f"{obj.pemilik.first_name} {obj.pemilik.last_name}"
+
+        return obj.pemilik.username
