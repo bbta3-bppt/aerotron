f418ac0dbf7c3294b6a810a38e4844c53065b7cd
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-27 17:22:55 +0700

    Penyesuaian fungsi interaksi bagian paket pengujian

--- templates/tronbase/src/pages/pengujian/RincianPengujian/rincian.js
+++ templates/tronbase/src/pages/pengujian/RincianPengujian/rincian.js
@@ -1,8 +1,7 @@
 import {onMounted, ref} from "vue"
 import {useStore} from "vuex"
 import {useRouter, useRoute} from "vue-router"
-import {getAllPengujian} from "src/services/pengujian"
-import {getAllPinjamanTertentu} from "src/services/pinjaman"
+import {getAllPaketPengujian, getAllPengujian} from "src/services/pengujian"
 
 export default {
   name: 'RincianPengujianPage',
@@ -11,8 +10,11 @@ export default {
     const route = useRoute()
     const router = useRouter()
     const pengujian = ref("")
+    const paket = ref("")
     const selectedPengujian = ref(false)
+    const idSelectedPengujian = ref(0)
     const pg = ref(2)
+    const pgPaket = ref(2)
 
     onMounted(async () => {
       await getAllPengujian(store, router, 1, route.params["id"])
@@ -24,10 +26,21 @@ export default {
       pg.value += 1
     }
 
+    const onRincianPengujian = async (id) => {
+      await getAllPaketPengujian(store, router, 1, id)
+      paket.value = store.getters["pengujian/paketPengujianGetter"]
+      selectedPengujian.value = true
+      idSelectedPengujian.value = id
+    }
+
     return {
       pengujian,
       selectedPengujian,
-      onMemuatPengujian
+      idSelectedPengujian,
+      paket,
+      pgPaket,
+      onMemuatPengujian,
+      onRincianPengujian
     }
   }
 }
