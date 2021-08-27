a31791a434f6107fb9bdcab3edc58b902bb71bbc
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-27 16:07:11 +0700

    Menerapkan action state yang baru untuk filter pengujian

--- templates/tronbase/src/services/pengujian.js
+++ templates/tronbase/src/services/pengujian.js
@@ -1,15 +1,16 @@
 import {refreshToken} from "src/services/refresh"
 import {Notify} from "quasar"
 
-export const getAllPengujian = async (store, router, page=1) => {
+export const getAllPengujian = async (store, router, page=1, id) => {
   const st = store
   const rt = router
   const pg = page
+  const i = id
 
   try {
     if (page === 1) store.commit("pengujian/resetAllPengujianMutation")
 
-    const res = await st.dispatch("pengujian/getPengujianAction", {page: pg})
+    const res = await st.dispatch("pengujian/getPengujianAction", {page: pg, id: i})
     store.commit("pengujian/setAllPengujianMutation", {
       count: res.data["count"] ? res.data["count"] : 0,
       results: res.data["results"] ? res.data["results"] : [],
@@ -23,7 +24,7 @@ export const getAllPengujian = async (store, router, page=1) => {
 
     if (err.response) {
       await refreshToken(st, null, rt)
-      await getAllPengujian(st, rt, pg)
+      await getAllPengujian(st, rt, pg, i)
     }
 
     else {
