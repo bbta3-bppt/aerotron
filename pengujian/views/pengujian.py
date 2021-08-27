439243508d54f74b8b2253b49c49882ebd3489ec
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-27 16:09:03 +0700

    Filter pengujian berdasarkan fasilitas di backend

--- pengujian/views/pengujian.py
+++ pengujian/views/pengujian.py
@@ -1,9 +1,17 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 
 from pengujian.models.pengujian import Pengujian
 from pengujian.serializers.pengujian import PengujianSerializers
+from perawatan.models.fasilitas import Fasilitas
 
 
 class DaftarPengujian(generics.ListAPIView):
     serializer_class = PengujianSerializers
     queryset = Pengujian.objects.all()
+
+    def get_queryset(self):
+        fasilitas_pk = self.kwargs.get("fasilitas_pk")
+        fasilitas_tertentu = get_object_or_404(Fasilitas, pk=fasilitas_pk)
+
+        return Pengujian.objects.filter(fasilitas=fasilitas_tertentu)
