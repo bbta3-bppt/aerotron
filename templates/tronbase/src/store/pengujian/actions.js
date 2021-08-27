ab95977c9d231131c775f2b58c61ff19315642e9
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-27 17:21:01 +0700

    Menambahkan action state ambil data paket pengujian

--- templates/tronbase/src/store/pengujian/actions.js
+++ templates/tronbase/src/store/pengujian/actions.js
@@ -14,3 +14,17 @@ export async function getPengujianAction (ctx, {page, id}) {
     }
   )
 }
+
+export async function getPaketPengujianAction (ctx, {page, id}) {
+  const header = Cookies.get("_msk")
+
+  return await api.get(
+    `pengujian/api/daftar/${id}/rincian`,
+    {
+      headers: { authorization: `Bearer ${header}` },
+      params: {
+        page: page != null ? page : null,
+      }
+    }
+  )
+}
