b64af3fc7507915ff1740f446241690474226847
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-27 17:27:14 +0700

    Membuat state untuk paket pengujian

--- templates/tronbase/src/store/pengujian/state.js
+++ templates/tronbase/src/store/pengujian/state.js
@@ -1,5 +1,6 @@
 export default function () {
   return {
     pengujian: {count: 0, results: [], next: null, previous: null},
+    paket: {count: 0, results: [], next: null, previous: null},
   }
 }
