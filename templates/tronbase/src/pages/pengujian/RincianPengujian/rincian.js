65871996bf3629dd92e8e5ca10ffbd226ba029ad
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-27 16:10:12 +0700

    Penyesuaian fungsi interaksi halaman pengujian sesuai fasilitas

--- templates/tronbase/src/pages/pengujian/RincianPengujian/rincian.js
+++ templates/tronbase/src/pages/pengujian/RincianPengujian/rincian.js
@@ -1,25 +1,26 @@
 import {onMounted, ref} from "vue"
 import {useStore} from "vuex"
-import {useRouter} from "vue-router"
+import {useRouter, useRoute} from "vue-router"
 import {getAllPengujian} from "src/services/pengujian"
-import {getAllPinjamanTertentu} from "src/services/pinjaman";
+import {getAllPinjamanTertentu} from "src/services/pinjaman"
 
 export default {
   name: 'RincianPengujianPage',
   setup() {
     const store = useStore()
+    const route = useRoute()
     const router = useRouter()
     const pengujian = ref("")
     const selectedPengujian = ref(false)
     const pg = ref(2)
 
     onMounted(async () => {
-      await getAllPengujian(store, router, 1)
+      await getAllPengujian(store, router, 1, route.params["id"])
       pengujian.value = store.getters["pengujian/pengujianGetter"]
     })
 
     const onMemuatPengujian = async () => {
-      await getAllPengujian(store, router, pg.value)
+      await getAllPengujian(store, router, pg.value, route.params["id"])
       pg.value += 1
     }
 
