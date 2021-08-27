8d8e609ff351ccb3de98bd4234ba318879c27888
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-27 17:27:33 +0700

    Koreksi lanjut url paket pengujian di backend

--- pengujian/urls.py
+++ pengujian/urls.py
@@ -5,5 +5,6 @@ from pengujian.views.pengujian import DaftarPengujian
 
 urlpatterns = [
     path('api/daftar/<int:fasilitas_pk>', DaftarPengujian.as_view(), name='daftar_pengujian'),
-    path('api/daftar/<int:fasilitas_pk>/rincian', DaftarPaketPengujian.as_view(), name='paket_pengujian'),
+    path('api/daftar/<int:pengujian_pk>/rincian/', DaftarPaketPengujian.as_view(),
+         name='paket_pengujian'),
 ]
