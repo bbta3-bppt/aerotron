0a33a40ab5b9fb4a41c8ff6734b77d6a520da970
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-27 16:10:48 +0700

    Koreksi url untuk pengujian masing-masing fasilitas

--- pengujian/urls.py
+++ pengujian/urls.py
@@ -4,6 +4,6 @@ from pengujian.views.paket import DaftarPaketPengujian
 from pengujian.views.pengujian import DaftarPengujian
 
 urlpatterns = [
-    path('api/daftar/', DaftarPengujian.as_view(), name='daftar_pengujian'),
-    path('api/daftar/<int:pengujian_pk>', DaftarPaketPengujian.as_view(), name='paket_pengujian'),
+    path('api/daftar/<int:fasilitas_pk>', DaftarPengujian.as_view(), name='daftar_pengujian'),
+    path('api/daftar/<int:fasilitas_pk>/rincian', DaftarPaketPengujian.as_view(), name='paket_pengujian'),
 ]
