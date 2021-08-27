6ea9d85de4fad5b75d4bce4a9257126734e2a751
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-27 17:22:05 +0700

    Membuat servis dengan fungsi ambil data paket pengujian

--- templates/tronbase/src/services/pengujian.js
+++ templates/tronbase/src/services/pengujian.js
@@ -37,3 +37,40 @@ export const getAllPengujian = async (store, router, page=1, id) => {
     }
   }
 }
+
+export const getAllPaketPengujian = async (store, router, page=1, id) => {
+  const st = store
+  const rt = router
+  const pg = page
+  const i = id
+
+  try {
+    if (page === 1) store.commit("pengujian/resetAllPaketPengujianMutation")
+
+    const res = await st.dispatch("pengujian/getPaketPengujianAction", {page: pg, id: i})
+    store.commit("pengujian/setAllPaketPengujianMutation", {
+      count: res.data["count"] ? res.data["count"] : 0,
+      results: res.data["results"] ? res.data["results"] : [],
+      next: res.data["next"] ? res.data["next"] : null,
+      previous: res.data["previous"] ? res.data["previous"] : null
+    })
+  }
+
+  catch (err) {
+    let message
+
+    if (err.response) {
+      await refreshToken(st, null, rt)
+      await getAllPengujian(st, rt, pg, i)
+    }
+
+    else {
+      message = err.message
+
+      Notify.create({
+        type: "negative",
+        message: message
+      })
+    }
+  }
+}
