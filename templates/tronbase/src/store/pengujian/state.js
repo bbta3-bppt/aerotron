54fc632da9a55a179a007aa16181cb4089a16991
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-28 18:38:18 +0700

    Membuat state untuk seleksi pengujian

--- templates/tronbase/src/store/pengujian/state.js
+++ templates/tronbase/src/store/pengujian/state.js
@@ -1,6 +1,7 @@
 export default function () {
   return {
     pengujian: {count: 0, results: [], next: null, previous: null},
+    pengujian_tertentu: {},
     paket: {count: 0, results: [], next: null, previous: null},
   }
 }
