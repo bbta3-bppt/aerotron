3927a709efaccee372df96e4bcc0877e1d758b17
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-28 18:37:07 +0700

    Membuat fungsi getter bagian seleksi pengujian

--- templates/tronbase/src/store/pengujian/getters.js
+++ templates/tronbase/src/store/pengujian/getters.js
@@ -2,6 +2,10 @@ export function pengujianGetter (state) {
   return state.pengujian
 }
 
+export function pengujianTertentuGetter (state) {
+  return state.pengujian_tertentu
+}
+
 export function paketPengujianGetter (state) {
   return state.paket
 }
