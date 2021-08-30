d7f2038cfa9849fb6454ac57afda279eb73dd7de
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-30 10:41:06 +0700

    Menambahkan parameter model database menjadi manual kapan dibuat

--- pengujian/models/pengujian.py
+++ pengujian/models/pengujian.py
@@ -10,8 +10,8 @@ class Pengujian(models.Model):
     deskripsi = models.TextField(blank=True)
     fasilitas = models.ForeignKey(Fasilitas, on_delete=models.CASCADE)
     pemilik = models.ForeignKey(User, on_delete=models.CASCADE)
+    dibuat = models.DateTimeField(default=timezone.now())
     berakhir = models.DateTimeField(default=timezone.now() + timezone.timedelta(days=180))
-    dibuat = models.DateTimeField(auto_now_add=True)
     diubah = models.DateTimeField(auto_now=True)
 
     class Meta:
